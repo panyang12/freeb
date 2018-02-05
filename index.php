c842a5301513f26ef3f4f0cb688b4a65082e5c5f
panyang12 <36172036+panyang12@users.noreply.github.com>
2018-02-06 04:22:06 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,67 @@
+<html>
+<head>
+<style>
+h1{
+ color:pink;
+ font-family:verdana;
+ font-size:200%;
+ text-align:center;
+}
+p1{
+ color:blue;
+ font-family:courier;
+ font-size:120%;
+}
+h2{
+ color:blue;
+ text:align:center;
+}
+p2{
+ color:green;
+
+}
+</style>
+</head>
+<body>
+
+<h1>BASIC INFORMATION</h1>
+
+<p1>Name:Stephanie V. Manloloyo<br>
+Address:Campaog,Pitalo,San Fernando,Cebu<br>
+Contact #:09369109479<br>
+Email:stephaniemanloloyo4@gmail.com<br>
+Age:22 year's old<br>
+Date of Birth:March 14,1995<br>
+Gender:Female<br>
+Hobbies:cooking and dancing<br>
+Favorite Color:black,white,blue and red<br>
+</p1><h2><center>EDUCATIONAL BACKGROUND</h2><p2><center>
+<table style="width:100%";color:blue;>
+<tr>
+<th>School</th>
+<th>School Address</th>
+<th>Year Graduated</th>
+</tr>
+<tr>
+<td><center>Pitalo Elementary School</center></td>
+<td><center>Pitalo,San Fernando,Cebu</center></td>
+<td><center>2006-2007</center></td>
+<tr>
+<td><center>Placido L. Senor National High School</center></td>
+<td><center>Langtad,City of Naga,Cebu</center></td>
+<td><center>2010-2011</center></td>
+</tr>
+</table>
+</p1><h2><center>BIOGRAPHY</h2><p2><center>
+I'm Stephanie V. Manloloy,22 years of age .Living in Campaog,Pitalo,San Fernando,Cebu.
+Currently studying in Cebu Technological University(Naga-Campus).I have five siblings three
+brother's and two sister's.My mother's name is Jonaliza Manloloyo she is a housewife.My fathers name is Emmanuel Manloloyo he is a motor driver.My hobbies are cooking and dancing.
+My favorite color are black,white,blue and red.<br>
+
+ 
+
+
+
+
+
+
